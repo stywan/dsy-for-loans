ec362e5adb3cd1349f127003721d66b1241b23dc
Victor Bello <stywan.v@gmail.com>
2025-05-11 22:15:21 -0400

    Implementa LoanService con lógica de negocio base para gestión de préstamos

---
+++ src/main/java/com/microservice/library/service/LoanService.java
@@ -0,0 +1,71 @@
+package com.microservice.library.service;
+
+import com.microservice.library.DTO.BookDTO;
+import com.microservice.library.DTO.LoanRequestDTO;
+import com.microservice.library.DTO.LoanResponseDTO;
+import com.microservice.library.DTO.UserDTO;
+import com.microservice.library.client.RestClientService;
+import com.microservice.library.exception.ResourceNotFoundException;
+import com.microservice.library.model.Loan;
+import com.microservice.library.repository.LoanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LoanService {
+    private final LoanRepository loanRepository;
+    private final RestClientService restClientService;
+    public LoanResponseDTO createLoan(LoanRequestDTO dto) {
+
+        UserDTO user = restClientService.getUserById(dto.getUserId());
+        BookDTO book = restClientService.getBookById(dto.getBookId());
+
+
+        Loan loan = new Loan();
+        loan.setUserId(dto.getUserId());
+        loan.setBookId(dto.getBookId());
+        loan.setLoanDate(dto.getLoanDate());
+        loan.setDueDate(dto.getDueDate());
+        loan.setStatus(Loan.LoanStatus.A);
+
+        loan = loanRepository.save(loan);
+
+        return mapToResponse(loan, user, book);
+    }
+
+    public LoanResponseDTO getLoanById(Long id) {
+        Loan loan = loanRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Loan not found"));
+
+        UserDTO user = restClientService.getUserById(loan.getUserId());
+        BookDTO book = restClientService.getBookById(loan.getBookId());
+
+        return mapToResponse(loan, user, book);
+    }
+
+    private LoanResponseDTO mapToResponse(Loan loan, UserDTO user, BookDTO book) {
+        return new LoanResponseDTO(
+                loan.getLoanId(),
+                loan.getLoanDate(),
+                loan.getDueDate(),
+                loan.getReturnDate(),
+                loan.getStatus().name(),
+                user,
+                book
+        );
+    }
+
+    public List<Loan> getAllLoans() {
+        return loanRepository.findAll();
+    }
+
+    public void deleteLoan(Long id) {
+        if (!loanRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Loan not found");
+        }
+        loanRepository.deleteById(id);
+    }
+}
